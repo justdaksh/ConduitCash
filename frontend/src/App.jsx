a5b45e399b14375c3632bad49dbd13fae5a36742
justdaksh <sdaksh31@gmail.com>
2024-07-26 21:05:03 +0530

    Setting Up tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { SignUp } from "./components/SignUp";
+import { Login } from "./components/Login";
+import { Dashboard } from "./components/Dashboard";
+import { SendMoney } from "./components/SendMoney";
+import { RecoilRoot } from "recoil";
 
 function App() {
-
   return (
-    <div>
-        SIKE!! Site Under Construction..
-    </div>
-  )
+    <RecoilRoot>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
+  );
 }
 
-export default App
+export default App;
