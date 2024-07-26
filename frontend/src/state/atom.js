0fe5e51b791365f2a00fe8b6741191ececfcc259
justdaksh <sdaksh31@gmail.com>
2024-07-26 21:09:29 +0530

    Recoil states added

---
+++ frontend/src/state/atom.js
@@ -0,0 +1,65 @@
+import { atom, selector } from "recoil";
+
+export const usernameAtom = atom({
+  key: "username",
+  default: "",
+});
+
+export const firstnameAtom = atom({
+  key: "firstnameAtom",
+  default: "",
+});
+
+export const lastnameAtom = atom({
+  key: "lastnameAtom",
+  default: "",
+});
+
+export const emailAtom = atom({
+  key: "emailAtom",
+  default: "",
+});
+
+export const passwordAtom = atom({
+  key: "passwordAtom",
+  default: "",
+});
+
+export const numberAtom = atom({
+    key: "numberAtom",
+    default: "",
+})
+
+export const userIdAtom = atom({
+    key: 'userId',
+    default: localStorage.getItem('userId') || null,
+  });
+
+export const balanceAtom = atom({
+    key: 'balanceAtom',
+    default: null,
+});
+
+export const tokenAtom = atom({
+    key: 'tokenAtom',
+    default: localStorage.getItem('token') || '',
+  });
+
+export const userNameSelector = selector({
+    key: 'userNameSelector',
+    get: ({ get }) => {
+      const userId = get(userIdAtom);
+      // This is a placeholder. In a real app, you'd fetch the user's name based on the userId
+      return userId ? 'User' : 'Guest';
+    },
+  });
+
+export const sendToAtom = atom({
+    key:"sendToAtom",
+    default:""
+})
+
+export const sendAmountAtom = atom({
+    key:"sendAmountAtom",
+    default:""
+})
