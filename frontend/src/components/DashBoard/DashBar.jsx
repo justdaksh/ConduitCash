2354f67a5485a7f1f03f4c4d822697f2192c1edc
justdaksh <sdaksh31@gmail.com>
2024-07-31 23:50:58 +0530

    adding Logout button and refractoring components

--- frontend/src/components/DashBoard/DashBar.jsx
+++ frontend/src/components/DashBoard/DashBar.jsx
@@ -1,12 +1,12 @@
-import axios from "axios";
-import { useCallback, useEffect } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
+
+import { useRecoilValue } from "recoil";
 import {
-  balanceAtom,
   firstnameAtom,
   lastnameAtom,
   usernameAtom,
 } from "../../state/atom";
+import { BalanceComponent } from "./BalanceComponent";
+import { Logout } from "../Auth/Logout";
 
 export function DashBar() {
   const firstname = useRecoilValue(firstnameAtom);
@@ -26,62 +26,11 @@ export function DashBar() {
             {firstname.substring(0, 1).toUpperCase() +
               lastname.substring(0, 1).toUpperCase()}
           </div>
+          <div>
+            <Logout />
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
-
-function BalanceComponent() {
-  const [balance, setBalance] = useRecoilState(balanceAtom);
-  const fetchBalance = useCallback(async () => {
-    try {
-      const response = await axios.get(
-        `${import.meta.env.VITE_API_BASE_URL}account/balance`,
-        {
-          headers: {
-            Authorization: localStorage.getItem("token") || null,
-          },
-        }
-      );
-      setBalance(response.data.balance);
-    } catch (error) {
-      console.error("Axios Error:", error);
-      setBalance("Connection Error");
-    }
-  }, [setBalance]);
-
-  useEffect(() => {
-    fetchBalance();
-  }, [fetchBalance]);
-
-  return (
-    <div className="flex items-center bg-gray-100 rounded-lg px-4 py-2">
-      <div className="font-semibold text-gray-700 mr-2">Your Balance:</div>
-      {balance !== null ? (
-        <div className="text-xl font-bold text-gray-800">₹{balance}</div>
-      ) : (
-        <div className="text-xl font-medium text-gray-500">Loading...</div>
-      )}
-      <button
-        onClick={fetchBalance}
-        className="ml-2 p-1 rounded-full hover:bg-gray-200 transition duration-300"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="h-5 w-5 text-gray-600"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-          />
-        </svg>
-      </button>
     </div>
   );
 }
