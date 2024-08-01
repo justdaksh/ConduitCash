82ec2bcbf245f130223c752afc2ff9bf6b2cefab
justdaksh <sdaksh31@gmail.com>
2024-08-01 22:13:14 +0530

    Mobile responsive Dashboard

--- frontend/src/components/DashBoard/DashBar.jsx
+++ frontend/src/components/DashBoard/DashBar.jsx
@@ -11,12 +11,30 @@ export function DashBar() {
   return (
     <div className="bg-white rounded-xl shadow-lg p-6">
       <div className="flex justify-between items-center">
-      <div className="font-bold text-2xl text-gray-800">ConduitCash</div>
+        <div className="hidden md:block font-bold text-2xl text-gray-800">
+          ConduitCash
+        </div>
+        <div className="md:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-7 h-7"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+            />
+          </svg>
+        </div>
         <div className="flex-1 flex justify-center">
           <BalanceComponent />
         </div>
         <div className="flex items-center space-x-4">
-          <div className="font-semibold text-xl text-gray-700">
+          <div className="hidden md:block font-semibold text-xl text-gray-700">
             Hello, {username}
           </div>
           <div className="w-10 h-10 rounded-full bg-gray-800 text-white flex items-center justify-center text-lg font-semibold">
